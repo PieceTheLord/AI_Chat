5ddeda46b645a27a63333edd13b9f103be7e3b6d
PieceTheLord <alwx202@mail.ru>
2025-03-29 21:36:50 +0300

    add `NEXT_URL` for the env vars

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,6 @@ const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
-  
 }
  
 const withMDX = createMDX({
